da2a397aba5663368c5582622a53769c3b87398a
MinJeongHoo <msw1302@gmail.com>
2020-11-15 12:23:53 +0900

    test

--- lists/migrations/0003_list_user.py
+++ lists/migrations/0003_list_user.py
@@ -10,14 +10,14 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('reviews', '0002_review_room'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('lists', '0002_list_rooms'),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='review',
+            model_name='list',
             name='user',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='reviews', to=settings.AUTH_USER_MODEL),
+            field=models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='list', to=settings.AUTH_USER_MODEL),
         ),
     ]
