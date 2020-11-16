a89af82d39a24fedbae86413930f05990a6576a6
MinJeongHoo <msw1302@gmail.com>
2020-11-16 09:25:01 +0900

    test

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -168,3 +170,15 @@ LOGIN_URL = "/users/login"
 
 # Locale
 LOCALE_PATHS = (os.path.join(BASE_DIR, "locale"),)
+
+# Sentry
+
+if not DEBUG:
+    sentry_sdk.init(
+        dsn="https://19a75bafaab54b52a1527ada9a152a2c@o477102.ingest.sentry.io/5517527",
+        integrations=[DjangoIntegration()],
+        traces_sample_rate=1.0,
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True,
+    )
