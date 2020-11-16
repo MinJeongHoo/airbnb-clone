bec77e31c63732ad0b4d779aa0a608b6f8ec1b88
MinJeongHoo <msw1302@gmail.com>
2020-11-16 11:09:43 +0900

    test

--- users/management/commands/createsu.py
+++ users/management/commands/createsu.py
@@ -6,16 +6,14 @@ class Command(BaseCommand):
 
     help = "This command creates many superuser"
 
+  class Command(BaseCommand):
+    
+    help = "This command creates superuser"
+
     def handle(self, *args, **options):
         admin = User.objects.get_or_none(username="ebadmin")
         if not admin:
-
-            User.objects.create_user(
-                username="ebadmin",
-                email="msw1302@gmail.com",
-                password="123456",
-            )
-            self.stdout.write(self.style.SUCCESS("Superuser Created"))
-
+            User.objects.create_superuser("ebadmin", "msw1302@gmail.com", "123456")
+            self.stdout.write(self.style.SUCCESS(f"Superuser Created"))
         else:
-            self.stdout.write(self.style.SUCCESS("Superuser Exists"))
+            self.stdout.write(self.style.SUCCESS(f"Superuser Exists"))
