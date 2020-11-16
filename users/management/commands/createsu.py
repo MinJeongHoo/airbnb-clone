14474e01fa03f26a577f665355b95ec0fb0331be
MinJeongHoo <msw1302@gmail.com>
2020-11-16 10:09:55 +0900

    test

--- users/management/commands/createsu.py
+++ users/management/commands/createsu.py
@@ -11,7 +11,9 @@ class Command(BaseCommand):
         if not admin:
 
             User.objects.create_superuser(
-                username="ebadmin", email="msw1302@gmail.com", password="123456"
+                username="ebadmin",
+                email="msw1302@gmail.com",
+                password="123456",
             )
             self.stdout.write(self.style.SUCCESS("Superuser Created"))
 
